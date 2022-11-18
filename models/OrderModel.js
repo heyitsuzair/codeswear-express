68a5075d650b591998c2b4a254ac106e203a28b8
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-11-19 02:04:48 +0500

    Models Added

---
+++ models/OrderModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const orderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    address: { type: String, required: true },
+    amount: { type: Number, required: true },
+    status: { type: String, required: true, default: "Pending" },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("orders", orderSchema);
